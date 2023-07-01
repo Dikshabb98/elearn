73dff78b2d5679945dabbf3752264a411114a314
Dikshabb98 <98941422+Dikshabb98@users.noreply.github.com>
2023-07-01 15:52:51 +0530

    succefully created simple navbar

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar'
+function App()  {
+  return (
+    <div className='nav'>
+      <Navbar/>
+    </div>
+     
+   
+  )
+}
+
+export default App
