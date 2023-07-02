f637de4ab83e58db4b8dc920665372bb4c8d0ae0
Dikshabb98 <98941422+Dikshabb98@users.noreply.github.com>
2023-07-02 21:27:19 +0530

    learning

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Image from '../images/picture1.png'
 const Home = () => {
   return (
-    <section className='hm'>
+   
+   <>
+   <section className='hm'>
         <h2>Welcome to Home page</h2>
     </section>
+    <img src={Image} alt="Image" />
+    </>
   )
 }
 
---
+++ my-app/src/components/Menulist.js
@@ -0,0 +1,18 @@
+export const Menulist =[
+    {
+        title: "Home",
+        url:"/"
+    },
+    {
+        title: "Course",
+        url:"/course"
+    },
+    {
+        title: "About",
+        url:"/about"
+    },
+    {
+        title: "Login",
+        url:"/login"
+    }
+]
