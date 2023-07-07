09945f03372ed350693213a453c316bf8036f72d
Dikshabb98 <98941422+Dikshabb98@users.noreply.github.com>
2023-07-07 23:07:09 +0530

    learning

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css'
 import React from 'react'
+import { BrowserRouter,Routes,  Route} from "react-router-dom"
 import Navbar from './components/Navbar'
 import MyImage from './components/MyImage'
-import Animation from "./components/Animation"
+import Home from './components/Home'
+import Course from './components/Course'
+import Roadmaps from './components/Roadmaps'
+import About from './components/About'
+import Login from './components/Login'
 function App()  {
   return (
     <div>
       <Navbar/>
+      <BrowserRouter>
+<Navbar/>
+<Routes>
+   <Route  path="/" element={<Home></Home>}/>
+   <Route  path="/Course" element={<Course></Course>}/>
+   <Route  path="/Roadmaps" element={<Roadmaps></Roadmaps>}/>
+   <Route  path="/About" element={<About></About>}/>
+   <Route  path="/Login" element={<Login></Login>}/>
+</Routes>
+</BrowserRouter>
       <MyImage/>
-      <Animation/>
+      
       </div>
    
      
